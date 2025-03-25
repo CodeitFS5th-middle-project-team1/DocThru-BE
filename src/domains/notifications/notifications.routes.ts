c38be71433a89c192fe2cc380bf48161f06cd0f2
hyuk-dev <hyuk.development@gmail.com>
2025-03-25 15:39:15 +0900

    re

--- src/domains/notifications/notifications.routes.ts
+++ src/domains/notifications/notifications.routes.ts
@@ -2,4 +2,6 @@ import { Router } from "express";
 
 const router = Router();
 
+router.post("/") // 알림 생성
+
 export default router;
--- src/domains/translations/translations.routes.ts
+++ src/domains/translations/translations.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-
+import FeedbackRouter from '../feedbacks/feedbacks.routes';
+import LikeRouter from '../likes/likes.routes';
 const router = Router();
 
 router.get("/") // 작업물 목록
 router.post("/") // 작업물 생성
 router.patch("/:translationId") // 작업물 수정
 router.delete("/:translationId") // 작업물 삭제
+router.post("/draftedTranslations") // 작업물 임시 저장
+router.get("/draftedTranslations") // 작업물 임시 저장 가져오기
+router.use("/:translationId/feedbacks", FeedbackRouter)
+router.use("/:translationId", LikeRouter);
 
 export default router;
