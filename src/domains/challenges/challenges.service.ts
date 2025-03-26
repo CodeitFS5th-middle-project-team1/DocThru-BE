70eab1664446cdba2d8d2480610dba95c60df510
hyuk-dev <hyuk.development@gmail.com>
2025-03-26 18:44:57 +0900

    feat: totalCount 추가

--- src/domains/challenges/challenges.service.ts
+++ src/domains/challenges/challenges.service.ts
@@ -9,6 +9,7 @@ const getChallenges = async (
   page: number,
   limit: number
 ) => {
+
   const skip = (page - 1) * limit;
   const fieldCondition =
     Array.isArray(fields) && fields.length > 0
@@ -16,23 +17,42 @@ const getChallenges = async (
       : fields
       ? { equals: fields as FieldType }
       : undefined;
-  const challenges = await prisma.challenge.findMany({
-    where: {
-      documentType: documentType || undefined,
-      field: fieldCondition || undefined,
-      approvalStatus,
-      ...(keyword && {
-        OR: [
-          { title: { contains: keyword, mode: 'insensitive' } },
-          { description: { contains: keyword, mode: 'insensitive' } },
-        ],
-      }),
-    },
-    skip,
-    take: limit,
-  });
 
-  return challenges;
+  
+  
+  const [challenges, totalCount] = await Promise.all([
+    prisma.challenge.findMany({
+      where: {
+        documentType: documentType || undefined,
+        field: fieldCondition || undefined,
+        approvalStatus,
+        ...(keyword && {
+          OR: [
+            { title: { contains: keyword, mode: 'insensitive' } },
+            { description: { contains: keyword, mode: 'insensitive' } },
+          ],
+        }),
+      },
+      skip,
+      take: limit,
+    }),
+    prisma.challenge.count({
+      where: {
+        documentType: documentType || undefined,
+        field: fieldCondition || undefined,
+        approvalStatus,
+        ...(keyword && {
+          OR: [
+            { title: { contains: keyword, mode: 'insensitive' } },
+            { description: { contains: keyword, mode: 'insensitive' } },
+          ],
+        }),
+      },
+    })
+  ])
+  
+
+  return {challenges, totalCount};
 };
 
 const ChallengesService = {
