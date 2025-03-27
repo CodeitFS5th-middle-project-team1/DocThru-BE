3d6a87bdde4dc00644e60bac2d961959fd779307
silverraining <www.stella.choi@gmail.com>
2025-03-27 14:43:45 +0900

    '

--- src/types/express.ts
+++ src/types/express.ts
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import { Request, Response, NextFunction } from 'express';
-=======
-import { NextFunction, Request, Response } from "express";
->>>>>>> develop
 
 export type Controller = (
   req: Request,
   res: Response,
   next: NextFunction
-<<<<<<< HEAD
 ) => Promise<void>;
-=======
-) => Promise<void>;
->>>>>>> develop
