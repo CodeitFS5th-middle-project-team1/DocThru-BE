b3ce9075ab2cc3211b3cbe61fbebe0aebe7b2436
stevenkim18 <11882@naver.com>
2025-03-28 18:17:07 +0900

    feat: Request 데이터 유효성 인증 미들웨어 구현, test api 생성

---
+++ src/domains/auth/auth.types.ts
@@ -0,0 +1,28 @@
+import z from 'zod';
+
+// TODO: 테스트용입니다. 나중에 삭제 예정입니다.
+export const testParamsSchema = z.object({
+  id: z.string().uuid({ message: 'id는 uuid 형식어아야 합니다.' }),
+});
+
+export const testQueriesSchema = z.object({
+  sort: z.enum(['desc', 'asc'], {
+    message: 'sort는 desc, asc 중 하나',
+  }),
+});
+
+export const testBodySchema = z.object({
+  email: z.string().email({ message: '이메일 형식을 지켜주세요.' }),
+  password: z.string().min(8, { message: '비번 8자리 이상' }),
+});
+
+export type testRequestParams = z.infer<typeof testParamsSchema>;
+export type testRequestQueries = z.infer<typeof testQueriesSchema>;
+export type testRequestBody = z.infer<typeof testBodySchema>;
+
+// 회원가입 API
+export const SignUpBodySchema = z.object({
+  email: z.string().email({ message: '이메일 형식을 지켜주세요.' }),
+  nickName: z.string(),
+  password: z.string().min(8, { message: '비밀번호는 8자리 이상' }),
+});
